39b7ab7b22d3c9cd99969df1d34a2947b94a07d4
Natalia Ahsan <121521544+ahsannatalia@users.noreply.github.com>
2026-08-29 14:39:18 -0300

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # for i in range(len(nums)):
+        #     lookingFor = target - nums[i]
+        #     print(f"Looking for the number: {lookingFor}")
+        #     for j in range(len(nums)):
+        #         if nums[j] == lookingFor and i != j:
+        #             print("found")
+        #             return [i, j]
+
+        indices = {}
+        for i, n in enumerate(nums):
+            indices[n] = i
+        
+        for i, n in enumerate(nums):
+            diff = target - nums[i]
+            if diff in indices and indices[diff] != i:
+                return [i, indices[diff]]
+        return []
+            
