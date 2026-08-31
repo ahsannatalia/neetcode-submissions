07b56f351df2ff3ff799eeed50f58ee9cdede33c
Natalia Ahsan <121521544+ahsannatalia@users.noreply.github.com>
2026-08-30 22:02:30 -0300

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,45 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+    #     stack = []
+    #     for i in range(len(s)):
+    #         if len(stack) != 0:
+    #             print(f"top of stack has:{stack[-1]}")
+    #             print(f"we are looking at: {s[i]}")
+    #             if self.isPair(stack[-1], s[i]):
+    #                 stack.pop()
+    #             else:
+    #                 print(f"adding: {s[i]} to the stack")
+    #                 stack.append(s[i])
+    #         else:
+    #             print(f"adding: {s[i]} to the stack")
+    #             stack.append(s[i])
+    #     if len(stack) == 0:
+    #         return True
+    #     return False
+            
+            
+
+    # def isPair(self, l: str, r: str):
+    #     if l == "[" and r == "]":
+    #         return True
+    #     elif l == "(" and r == ")":
+    #         return True
+    #     elif l == "{" and r == "}":
+    #         return True 
+    #     return False
+
+        stack = []
+        pairs = {")": "(", "]": "[", "}": "{"}
+        for c in s:
+            if c in pairs:
+                if stack and stack[-1] == pairs[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        # stack empty check
+        return True if not stack else False
+
+
+        
